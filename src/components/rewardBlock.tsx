044804e6e46129e9bc4dd0036233767476fc2706
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2023-09-16 13:06:03 +0530

    nft temp added

--- src/components/rewardBlock.tsx
+++ src/components/rewardBlock.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import templateReward from '../assets/Social/template.jpg';
+
 function RewardBlock() {
   return (
     <div className=" w-[220px] h-[300px] bg-white border border-black rounded-md flex justify-center items-center">
-      <div className=" w-[210px] h-[290px] bg-white border border-black rounded-md"></div>
+      <div className=" w-[210px] h-[290px] bg-white border border-black rounded-md flex justify-center items-center ">
+        <Image src={templateReward} alt="template reward" width={190} height={270} className=" rounded-md w-[200px] h-[280px]" />
+      </div>
     </div>
   );
 }
